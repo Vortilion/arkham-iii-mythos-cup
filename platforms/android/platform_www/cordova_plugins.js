40e2ecf93b2cbc8420ad4a6ed42cbac35d16ed64
Sascha Kleiber <sascha.kleiber@adesso-insurance-solutions.de>
2023-04-11 11:08:35 +0200

    added gh-pages update

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,41 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-globalization.GlobalizationError",
+      "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+      "pluginId": "cordova-plugin-globalization",
+      "clobbers": [
+        "window.GlobalizationError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-globalization.globalization",
+      "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+      "pluginId": "cordova-plugin-globalization",
+      "clobbers": [
+        "navigator.globalization"
+      ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-globalization": "1.11.0",
+    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-device": "2.0.3"
+  };
+});
---
+++ platforms/browser/www/js/data/mythos-tokens.js
@@ -0,0 +1,82 @@
+let mythosTokens = [
+	{
+		"name": {
+			"en": "Spread Doom",
+			"it": "Diffusione dei Destini",
+			"es": "Propagación de la Perdición",
+            "de": ""
+		
+        },
+		"icon": "doom"
+	},
+	{
+		"name": {
+			"en": "Spawn Monster",
+			"it": "Generazione di un Mostro",
+			"es": "Aparición de Monstruo",
+            "de": ""
+		
+        },
+		"icon": "monster"
+	},
+	{
+		"name": {
+			"en": "Spawn Clue",
+			"it": "Generazione di un Indizio",
+			"es": "Aparición de Pista",
+            "de": ""
+		
+        },
+		"icon": "clue"
+	},
+	{
+		"name": {
+			"en": "Read Headline",
+			"it": "Lettura di un Quotidiano",
+			"es": "Leer un Titular",
+            "de": ""
+		
+        },
+		"icon": "headline"
+	},
+	{
+		"name": {
+			"en": "Gate Burst",
+			"it": "Squarcio del Portale",
+			"es": "Estallido de portal",
+            "de": ""
+		
+        },
+		"icon": "gate-burst"
+	},
+	{
+		"name": {
+			"en": "Reckoning",
+			"it": "Resa dei conti",
+			"es": "Retribución",
+            "de": ""
+		
+        },
+		"icon": "reckoning"
+	},
+	{
+		"name": {
+			"en": "Blank",
+			"it": "Vuoto",
+			"es": "Vacía",
+            "de": ""
+		
+        },
+		"icon": "blank-empty"
+	},
+	{
+		"name": {
+			"en": "Spread Terror",
+			"it": "Diffusione del Terrore",
+			"es": "Propagacion del Terror",
+            "de": ""
+		
+        },
+		"icon": "terror"
+	}
+];
